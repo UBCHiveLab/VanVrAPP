f68e9c3e71febbcbff013def793530979c73b1a2
Alireza Shafiei <alireza.021@hotmail.com>
2020-06-08 15:47:07 -0700

    Landing scene created with start button animations

---
+++ Assets/Scripts/LandingPage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LandingPage: MonoBehaviour
+{
+    public Button startButton;
+	public GameObject doorL;
+	public GameObject doorR;
+	public GameObject doors;
+	public GameObject mainCamera;
+
+	void Start()
+	{
+		Button btn = startButton.GetComponent<Button>();
+		btn.onClick.AddListener(StartSession);
+	}
+
+	void StartSession() {
+		doors.GetComponent<Animator>().SetTrigger("Start");
+		mainCamera.GetComponent<Animator>().SetTrigger("Start");
+
+	}
+
+}
+
