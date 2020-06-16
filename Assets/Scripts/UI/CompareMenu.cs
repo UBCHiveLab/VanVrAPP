198d0fefa5c82f99c157f902acb5d5a7afe61cf0
Joshua Grant <jc.grant22@gmail.com>
2020-06-16 08:36:07 -0700

    Clean up and commenting.

--- Assets/Scripts/UI/CompareMenu.cs
+++ Assets/Scripts/UI/CompareMenu.cs
@@ -1,44 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Assets.Scripts.Controller;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CompareMenu : MonoBehaviour
 {
-
+    [Header("Services")]
     public SpecimenStore store;
     public StateController stateController;
     
+    [Header("Internal Structure")]
     public Button viewToggle;
     public SpecimenListing primarySpecimenListing;
     public SpecimenListing secondarySpecimenListing;
     public Transform addListTransform;
+
+    [Header("Prefabs")]
     public SpecimenListing addListingPrefab;
 
 
     void OnEnable()
     {
-        viewToggle.onClick.RemoveAllListeners();
-        viewToggle.onClick.AddListener(TogglePanel);
         Populate();
     }
 
-
     public void TogglePanel() {
-
         gameObject.SetActive(!gameObject.activeSelf);
-        Populate();
+        viewToggle.onClick.RemoveAllListeners();
+        viewToggle.onClick.AddListener(TogglePanel);
     }
 
     public void Populate()
     {
         Clear();
-        Debug.Log(stateController.CurrentSpecimenData);
+
+        if (stateController.CompareSpecimenData == null)
+        {
+            Debug.LogWarning("No specimen selected; not populating compare menu.");
+            return;
+        }
+
+        // Populates the primary specimen holder, including its "remove" button
         primarySpecimenListing.Populate(stateController.CurrentSpecimenData, () => {Remove(0);});
 
+        // Populates the compare specimen holder, if there is a compare specimen selected, including its "remove" button
         if (stateController.CompareSpecimenData != null)
         {
             secondarySpecimenListing.gameObject.SetActive(true);
@@ -49,6 +54,7 @@ public class CompareMenu : MonoBehaviour
             secondarySpecimenListing.gameObject.SetActive(false);
         }
 
+        // Generates the list of comparable specimens
         PopulateAddList();
     }
 
@@ -63,8 +69,10 @@ public class CompareMenu : MonoBehaviour
             stateController.RemoveCompareSpecimen();
         }
 
+        // If primary specimen is removed, compare specimen becomes the primary.
         secondarySpecimenListing.gameObject.SetActive(false);
 
+        // If a specimen remains, populates the ui again.
         if (stateController.CurrentSpecimenData != null)
         {
             Populate();
@@ -85,7 +93,7 @@ public class CompareMenu : MonoBehaviour
     {
         // TODO: filter this by whatever we choose (maybe same organ?)
 
-
+        // Filters given ids from specimen data list and populates those in the ui list.
         foreach (SpecimenData spd in store.GetSpecimenDataFiltered(new List<string>{stateController.currentSpecimenId, stateController.CompareSpecimenData?.Id}))
         {
             SpecimenListing specimen = Instantiate(addListingPrefab, addListTransform);
