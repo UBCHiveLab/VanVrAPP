1e529f0ba9706cb6b28134a31aa539fc33e00510
Alireza Shafiei <alireza.021@hotmail.com>
2020-06-28 15:06:59 -0700

    AssetBundles pipeline created

---
+++ Assets/Scripts/Editor/BundleOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BundleOptions : MonoBehaviour
+{
+    [MenuItem("Custom Utilities/Build StandaloneWindows")]
+    static void PerformBuild() {
+        BuildPipeline.BuildAssetBundles("../AssetBundles/", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+    }
+}
