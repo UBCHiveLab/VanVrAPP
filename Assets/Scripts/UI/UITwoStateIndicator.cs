65c72ce765fc8a4bb5baec96b10425e598ef13f4
Joshua Grant <jc.grant22@gmail.com>
2020-07-22 09:17:34 -0700

    Quick fix - action buttons

--- Assets/Scripts/UI/UITwoStateIndicator.cs
+++ Assets/Scripts/UI/UITwoStateIndicator.cs
@@ -1,16 +1,22 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UITwoStateIndicator : MonoBehaviour
 {
     public Sprite imageTrue;
     public Sprite imageFalse;
+    public string textTrue;
+    public string textFalse;
     public bool state;
     public Image frame;
+    public TextMeshProUGUI label;
+
 
     public void UpdateState(bool newState)
     {
         state = newState;
         frame.sprite = state ? imageTrue : imageFalse;
+        label.text = state ? textTrue : textFalse;
     }
 }
