f1226fe9dc50810beabb9cf21b4c88af105eb143
Joshua Grant <jc.grant22@gmail.com>
2020-07-21 17:50:51 -0700

    hover buttys!

---
+++ Assets/Scripts/UI/HoverButton.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float transitionTime = 0.25f;
+    public Color baseColor;
+    public Color hoverColor;
+
+    public Image icon;
+    public Image icon2;
+    public TextMeshProUGUI label;
+
+    private float current;
+
+
+
+    void Start()
+    {
+        if (transitionTime <= 0)
+        {
+            transitionTime = 0.1f;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(HoverOn());
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(HoverOff());
+    }
+
+
+    private IEnumerator HoverOn()
+    {
+        while (current < 1f)
+        {
+            current += Time.deltaTime / transitionTime;
+            Color color = Color.Lerp(baseColor, hoverColor, current);
+
+            if (label != null) {
+                label.color = color;
+            }
+
+            if (icon != null) {
+                icon.color = color;
+            }
+
+            if (icon2 != null)
+            {
+                icon2.color = color;
+            }
+
+            yield return null;
+        }
+
+        current = 1f;
+    }
+
+    private IEnumerator HoverOff()
+    {
+        while (current > 0f) {
+            current -= Time.deltaTime / transitionTime;
+            Color color = Color.Lerp(baseColor, hoverColor, current);
+
+            if (label != null) {
+                label.color = color;
+            }
+
+            if (icon != null) {
+                icon.color = color;
+            }
+
+            if (icon2 != null) {
+                icon2.color = color;
+            }
+
+            yield return null;
+        }
+
+        current = 0f;
+    }
+}
