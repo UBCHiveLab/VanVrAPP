0b44ba7613febdb178470c76909225947fb06a85
Joshua Grant <jc.grant22@gmail.com>
2020-07-28 09:59:24 -0700

    Working enter flow

--- Assets/Scripts/UI/StartButton.cs
+++ Assets/Scripts/UI/StartButton.cs
@@ -1,20 +1,63 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class StartButton : MonoBehaviour
+public class StartButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Action buttonAction;
 
-    private void OnMouseDown()
-    {
-        buttonAction();
-    }
+    public Image fillImage;
+    public float totalTouchTime = 1f;
+
+    private float currentTouchTime;
+    private bool touching;
+
+
+    
 
     public void bindAction(Action action)
     {
         buttonAction = action;
+    }
+
+    public void OnCompleteTouch()
+    {
+        buttonAction();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && touching)
+        {
+            currentTouchTime += Time.deltaTime;
+            if (currentTouchTime >= totalTouchTime)
+            {
+                buttonAction();
+            }
+        }
+        else
+        {
+            currentTouchTime = Mathf.Max(0, currentTouchTime - Time.deltaTime);
+        }
+
+        fillImage.fillAmount = currentTouchTime / totalTouchTime;
+    }
+
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        touching = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        touching = false;
     }
 }
