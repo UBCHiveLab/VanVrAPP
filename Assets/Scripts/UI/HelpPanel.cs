8f043f1ef0beb3bc055216a425ce5a9c57793bf5
Joshua Grant <jc.grant22@gmail.com>
2020-07-31 13:33:01 -0700

    Help panel roughed in

---
+++ Assets/Scripts/UI/HelpPanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Controller;
+using UnityEngine;
+
+public class HelpPanel : MonoBehaviour
+{
+    public StateController controller;
+
+    void OnEnable()
+    {
+        Debug.Log($"Opening help panel in {controller.mode} mode");
+    }
+}
