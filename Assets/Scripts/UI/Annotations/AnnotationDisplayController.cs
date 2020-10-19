15cff9109cbb92fce57ae85d836315e764b9e491
Steve <wentaozhang0305@gmail.com>
2020-10-19 15:34:34 -0700

    annotation display button control

---
+++ Assets/Scripts/UI/Annotations/AnnotationDisplayController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnnotationDisplayController : MonoBehaviour
+{
+    [Header("Buttons")]
+    public GameObject displayOn;
+    public GameObject displayOff;
+    public GameObject annotationDisplayControllerUI;
+
+    private AnnotationIndicator annotationIndicator;
+
+    public bool isAnnotationDisplayOn;
+    public bool isButtonOn;
+
+    void Start()
+    {
+        isButtonOn = true;
+        isAnnotationDisplayOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnnotationDisplayControllerStatement()
+    {
+        annotationDisplayControllerUI.SetActive(!isAnnotationDisplayOn);
+        isAnnotationDisplayOn = !isAnnotationDisplayOn;
+    }
+
+    public void AnnotationDisplayControllerButton()
+    {
+        isButtonOn = !isButtonOn;
+        displayOff.SetActive(!isButtonOn);
+        displayOn.SetActive(isButtonOn);
+    }
+}
