a297a4f7e837dbfd16cb44d9963a1cf647fc861b
rprakitpong <rprakitpong@gmail.com>
2021-10-27 22:59:08 -0700

    Updated display of MongoDB database courses in home page

---
+++ Assets/Scripts/TrialScripts/CourseDisplayOptions.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * The representation of a single lab course for the SelectorMenu
+ */
+public class CourseDisplayOptions : MonoBehaviour
+{
+    [Header("Internal Structure")]
+    public Button button;
+    public Text courseNameFrame2;
+
+    private CoursesPage _coursesPage;
+
+    public void Populate(string courseId, CoursesPage coursesPage)
+    {
+        courseNameFrame2.text = courseId;
+        _coursesPage = coursesPage;
+      //  button.onClick.AddListener(() => _coursesPage.CourseSelected(courseId));
+    }
+}
